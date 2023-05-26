28790b9fce2d59379932cbe8d21414f08da36f73
Giovanni Lombardo <glombardo.gl@gmail.com>
2023-05-26 16:20:50 +0200

    Create classi

---
+++ Progetto/src/main/Chat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author giovanni
+ */
+public abstract class Chat {
+    private ArrayList<Utente> partecipanti;
+    private ArrayList<Messaggio> messaggi;
+}
---
+++ Progetto/src/main/Gruppo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author giovanni
+ */
+public class Gruppo extends Chat{
+    private Utente amministratore;
+    private String nome;
+}
