43e0652ff42f1d5f28a8b4ef14118919b2da5d63
Giovanni Lombardo <glombardo.gl@gmail.com>
2023-06-02 18:16:50 +0200

    Creato package per i file lato server

--- Progetto/src/server/GestoreClient.java
+++ Progetto/src/server/GestoreClient.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main;
+package server;
 import java.io.*;
 import java.net.*;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import main.Database;
 /**
  *
  * @author matxd
