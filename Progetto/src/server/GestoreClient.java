7eef5c11a9253b76f562449408afa19845b0971a
Matteo Bonsignore <mat.bonsignore@gmail.com>
2023-06-07 20:29:17 +0200

    fix

--- Progetto/src/server/GestoreClient.java
+++ Progetto/src/server/GestoreClient.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package server;
-import server.Database;
 import java.io.*;
 import java.net.*;
 import java.sql.SQLException;
